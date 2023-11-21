fc4a2af6ef8784d2e6f73fdb7d85131cebe6d5a9
ASP <andrewpeshekhonov@gmail.com>
2023-11-21 12:57:45 +0200

    feat(docker-compose): done

--- src/api/settings.py
+++ src/api/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379')
